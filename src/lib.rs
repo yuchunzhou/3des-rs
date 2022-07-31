2676005d428252fe164bcd2b586cd95b43e2103d
yuchunzhou <909655299@qq.com>
2022-07-31 18:02:48 +0800

    fix function signature

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 extern "C" {
     pub fn des3_cbc_encrypt(
-        pout: *const u8,
+        pout: *mut u8,
         pdata: *const u8,
         nlen: u32,
         pkey: *const u8,
@@ -8,7 +8,7 @@ extern "C" {
         piv: *const u8,
     ) -> u32;
     pub fn des3_cbc_decrypt(
-        pout: *const u8,
+        pout: *mut u8,
         pdata: *const u8,
         nlen: u32,
         pkey: *const u8,
@@ -17,14 +17,14 @@ extern "C" {
     ) -> u32;
 
     pub fn des3_ecb_encrypt(
-        pout: *const u8,
+        pout: *mut u8,
         pdata: *const u8,
         nlen: u32,
         pkey: *const u8,
         klen: u32,
     ) -> u32;
     pub fn des3_ecb_decrypt(
-        pout: *const u8,
+        pout: *mut u8,
         pdata: *const u8,
         nlen: u32,
         pkey: *const u8,
