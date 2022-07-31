1893a6c8a1ae5add6c493b59798c193683474b4d
yuchunzhou <909655299@qq.com>
2022-07-31 20:09:42 +0800

    add a strip function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+// strip the suffix from a slice
+pub fn strip(s: &[u8], suffix: u8) -> &[u8] {
+    let mut s_ = s;
+    loop {
+        match s_.strip_suffix(&[suffix]) {
+            None => {
+                return &s_;
+            }
+            Some(s) => {
+                s_ = s;
+            }
+        }
+    }
+}
+
 extern "C" {
     pub fn des3_cbc_encrypt(
         pout: *mut u8,
