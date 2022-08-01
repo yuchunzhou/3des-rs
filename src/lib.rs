b3a12bb2da12200ed4795a56fe214d351aff1f41
yuchunzhou <909655299@qq.com>
2022-08-01 18:27:54 +0800

    make it safe

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,63 @@
-// strip the suffix from a slice
-pub fn strip(s: &[u8], suffix: u8) -> &[u8] {
-    let mut s_ = s;
-    loop {
-        match s_.strip_suffix(&[suffix]) {
-            None => {
-                return &s_;
-            }
-            Some(s) => {
-                s_ = s;
-            }
-        }
-    }
+extern "C" {
+    fn des3_cbc_encrypt(
+        pout: *mut u8,
+        pdata: *const u8,
+        nlen: u32,
+        pkey: *const u8,
+        klen: u32,
+        piv: *const u8,
+    ) -> u32;
+    fn des3_cbc_decrypt(
+        pout: *mut u8,
+        pdata: *const u8,
+        nlen: u32,
+        pkey: *const u8,
+        klen: u32,
+        piv: *const u8,
+    ) -> u32;
+
+    fn des3_ecb_encrypt(
+        pout: *mut u8,
+        pdata: *const u8,
+        nlen: u32,
+        pkey: *const u8,
+        klen: u32,
+    ) -> u32;
+    fn des3_ecb_decrypt(
+        pout: *mut u8,
+        pdata: *const u8,
+        nlen: u32,
+        pkey: *const u8,
+        klen: u32,
+    ) -> u32;
 }
 
-extern "C" {
-    pub fn des3_cbc_encrypt(
-        pout: *mut u8,
-        pdata: *const u8,
-        nlen: u32,
-        pkey: *const u8,
-        klen: u32,
-        piv: *const u8,
-    ) -> u32;
-    pub fn des3_cbc_decrypt(
-        pout: *mut u8,
-        pdata: *const u8,
-        nlen: u32,
-        pkey: *const u8,
-        klen: u32,
-        piv: *const u8,
-    ) -> u32;
+pub fn cbc_encrypt(
+    pout: *mut u8,
+    pdata: *const u8,
+    nlen: u32,
+    pkey: *const u8,
+    klen: u32,
+    piv: *const u8,
+) -> u32 {
+    unsafe { des3_cbc_encrypt(pout, pdata, nlen, pkey, klen, piv) }
+}
 
-    pub fn des3_ecb_encrypt(
-        pout: *mut u8,
-        pdata: *const u8,
-        nlen: u32,
-        pkey: *const u8,
-        klen: u32,
-    ) -> u32;
-    pub fn des3_ecb_decrypt(
-        pout: *mut u8,
-        pdata: *const u8,
-        nlen: u32,
-        pkey: *const u8,
-        klen: u32,
-    ) -> u32;
+pub fn cbc_decrypt(
+    pout: *mut u8,
+    pdata: *const u8,
+    nlen: u32,
+    pkey: *const u8,
+    klen: u32,
+    piv: *const u8,
+) -> u32 {
+    unsafe { des3_cbc_decrypt(pout, pdata, nlen, pkey, klen, piv) }
+}
+
+pub fn ecb_encrypt(pout: *mut u8, pdata: *const u8, nlen: u32, pkey: *const u8, klen: u32) -> u32 {
+    unsafe { des3_ecb_encrypt(pout, pdata, nlen, pkey, klen) }
+}
+
+pub fn ecb_decrypt(pout: *mut u8, pdata: *const u8, nlen: u32, pkey: *const u8, klen: u32) -> u32 {
+    unsafe { des3_ecb_decrypt(pout, pdata, nlen, pkey, klen) }
 }
