85f440d0ea5fa297e753baa0dfe6db196d891e73
yuchunzhou <909655299@qq.com>
2022-08-03 19:22:06 +0800

    update

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 fn main() {
+    println!("cargo:rerun-if-changed=src/3des.c");
+    println!("cargo:rerun-if-changed=src/3des.h");
+
     cc::Build::new()
         .file("src/3des.c")
         .include("src")
